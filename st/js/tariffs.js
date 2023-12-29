ea57294bb1d02c07a0f921bcd650f931435ec84f
Vlados <vladik00848@gmail.com>
2023-12-29 19:31:35 +0500

    1

--- st/js/tariffs.js
+++ st/js/tariffs.js
@@ -12,31 +12,6 @@ elements.forEach(function (element, index) {
     delay: index, // Задержка для последовательного запуска анимации для каждого элемента
   });
 });
-// ЭТО ФУНКЦИОНАЛ МОДАЛКИ ЗАЯВКИ
-
-// Функция открытия модального окна
-const openModalApplication = () => {
-  const modal = document.getElementById("modal");
-  modal.style.display = "block";
-  document.body.style.overflow = "hidden";
-};
-
-// Функция закрытия модального окна
-const closeModalApplication = () => {
-  const modal = document.getElementById("modal");
-  modal.style.display = "none";
-  document.body.style.overflow = "auto";
-};
-// ЭТО ФУНКЦИОНАЛ МОДАЛКИ ГОРОДОВ
-// Функция открытия модального окна
-const openModalCities = () => {
-  const modal = document.getElementById("cities");
-  if (modal.style.display == "none") {
-    modal.style.display = "flex";
-  } else {
-    modal.style.display = "none";
-  }
-};
 
 function CheckTariffs() {
   animationCards.restart();
@@ -52,7 +27,7 @@ function CheckTariffs() {
       activeProviders.push(element.name); // Убран доступ к childNodes, так как name прямо доступен у элемента
     }
   });
-  console.log(activeProviders)
+  console.log(activeProviders);
   var tariffsContainer = document.getElementById("tariffs");
   tariffsContainer?.childNodes.forEach((element) => {
     if (element.nodeType === 1) {
@@ -145,6 +120,57 @@ const animationCards = anime({
   easing: "easeInOutQuad",
   duration: 1000,
 });
+
+// ЭТО ФУНКЦИОНАЛ МОДАЛКИ ЗАЯВКИ
+// Функция открытия модального окна
+const openModalApplication = () => {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("modals-inactive");
+  modal.classList.add("modals-active");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  document.body.style.overflow = "hidden";
+};
+// Функция закрытия модального окна
+const closeModalApplication = () => {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("modals-active");
+  modal.classList.add("modals-inactive");
+  document.body.style.overflow = "auto";
+};
+
+// ЭТО ФУНКЦИОНАЛ МОДАЛКИ ГОРОДОВ
+const modal = document.getElementById("cities");
+// Определяем анимацию
+const animationStart = anime({
+  targets: modal,
+  opacity: [0, 1], // Переход от невидимости к видимости
+  translateY: [0, 0], // Смещение по оси Y
+  easing: "easeInOutQuad",
+  duration: 250,
+});
+const animationEnd = anime({
+  targets: modal,
+  opacity: [1, 0], // Переход от невидимости к видимости
+  translateY: [0, 0], // Смещение по оси Y
+  easing: "easeInOutQuad",
+  duration: 250,
+});
+// Функция открытия модального окна
+const openModalCities = () => {
+  const modal = document.getElementById("cities");
+  if (modal.style.display == "none") {
+    modal.style.display = "flex";
+    animationStart.restart();
+  } else {
+    animationEnd.restart();
+    setTimeout(() => {
+      modal.style.display = "none";
+    }, 250);
+  }
+};
 
 // menuMobile
 function menuMobile() {
